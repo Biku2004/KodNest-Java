32b17074d6eb053c9d9bf2804f709ac4d4897c18
Biku2004 <bikash.behera24.04.2004@gmail.com>
2026-07-30 12:32:06 +0530

    updated welcom.java file

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -5,6 +5,13 @@ public class WelcomeToJava {
         System.out.println("I already know how to build programming Login");
         System.out.println("Now I am learning how to express that logic using JAVA");        
 
+        System.out.println();
+
+        System.out.println("My name is Bikash");
+        System.out.println("My goal is to become a JAVA developer");
+        System.out.println("I selected JAVA because I want to build large applications");
+
+
     }
 }
 
