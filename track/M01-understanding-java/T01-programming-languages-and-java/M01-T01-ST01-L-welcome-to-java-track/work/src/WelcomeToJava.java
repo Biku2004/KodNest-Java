e8996f557915cc45d75e6f6eee3a560f2cd5e2b1
Biku2004 <bikash.behera24.04.2004@gmail.com>
2026-07-30 12:24:54 +0530

    feat : first track completed

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,10 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+       
+        System.out.println("welcome to JAVA track");
+        System.out.println("I already know how to build programming Login");
+        System.out.println("Now I am learning how to express that logic using JAVA");        
+
     }
 }
+
