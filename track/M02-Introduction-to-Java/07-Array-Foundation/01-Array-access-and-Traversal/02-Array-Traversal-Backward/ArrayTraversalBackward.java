2a45a7de42134cfd320ac59b2a4bf775a38c8cb7
Biku2004 <bikash.behera24.04.2004@gmail.com>
2026-08-17 12:49:55 +0530

    array traversal foward and reverse

---
+++ track/M02-Introduction-to-Java/07-Array-Foundation/01-Array-access-and-Traversal/02-Array-Traversal-Backward/ArrayTraversalBackward.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ArrayTraversalBackward {
+        public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int a[] = new int[5];
+
+        System.out.println("Enter Array elements");
+
+        for(int i=0; i<a.length; i++){
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("Array Elements are: ");
+
+        for(int i=a.length-1; i>=0;i--){
+            System.out.println(a[i]);
+        }
+
+        sc.close();
+
+    }
+}
